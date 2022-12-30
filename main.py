fe414bae431a0243b4638ef0bf78da17167fa1a6
Antony Praveenkumar M <antony.praveenkumar@softsuave.com>
2022-12-30 11:39:15 +0530

    snowflake

--- main.py
+++ main.py
@@ -50,3 +50,84 @@ conn.cursor().execute(
 
 # Loading Data
 # https://docs.snowflake.com/en/user-guide/python-connector-example.html#loading-data
+'''
+Instead of inserting data into tables using individual INSERT commands, 
+you can bulk load data from files staged in either an internal or external location.
+'''
+# Copying Data from an Internal Location
+# Putting Data
+# conn.cursor().execute("PUT file:///tmp/data/file* @%testtable")
+try:
+    conn.cursor().execute(
+        "CREATE OR REPLACE TABLE "
+        "mock_data(id integer, first_name string, last_name string, email string, gender string, ip_address string)")
+
+    conn.cursor().execute("PUT file:///home/softsuave/project/snowflake/tmp/data/MOCK_DATA.csv @%mock_data")
+    conn.cursor().execute("COPY INTO mock_data")
+except Exception as ex:
+    print(ex)
+
+# Copying Data from an External Location
+# https://docs.snowflake.com/en/user-guide/python-connector-example.html#copying-data-from-an-external-location
+try:
+    # Copying Data
+    conn.cursor().execute("""
+    COPY INTO testtable FROM s3://<s3_bucket>/data/
+        STORAGE_INTEGRATION = myint
+        FILE_FORMAT=(field_delimiter=',')
+    """.format(
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY))
+except Exception as ex:
+    print(ex)
+
+
+# Querying Data
+# https://docs.snowflake.com/en/user-guide/python-connector-example.html#querying-data
+'''
+With the Snowflake Connector for Python, you can submit:
+
+    1.a synchronous query, which returns control to your application after the query completes.
+
+    2.an asynchronous query, which returns control to your application before the query completes.
+
+After the query has completed, you use the Cursor object to fetch the values in the results. 
+By default, the Snowflake Connector for Python converts the values from Snowflake data types to native 
+Python data types. (Note that you can choose to return the values as strings and perform the type 
+conversions in your application. See Improving Query Performance by Bypassing Data Conversion.)
+'''
+# Performing a Synchronous Query
+# To perform a synchronous query, call the execute() method in the Cursor object. For example:
+cur = conn.cursor()
+try:
+    # we are already mentioned what database, warehouse and schema have to use
+    print(cur.execute('select * from mock_data'))
+except Exception as ex:
+    print(ex)
+
+
+# Performing an Asynchronous Query
+# count = cur.execute_async('select count(*) from table(generator(timeLimit => 25))')
+async_query = cur.execute_async('''ALTER TABLE mock_data
+                            ADD date_of_birth DATE ;''')
+
+# Checking the Status of a Query
+if conn.get_query_status(async_query.get('queryId')).name == 'SUCCESS':
+    print('query exceqution is finished')
+else:
+    print('Query exceqution is not finised')
+
+# To retrieve the results of the query
+# Get the results from a query.
+async_query = cur.execute_async('''select * from mock_data;''')
+cur.get_results_from_sfqid(async_query.get('queryId'))
+results = cur.fetchall()
+print(f'{results[0]}')
+
+
+# Retrieving the Snowflake Query ID
+# https://docs.snowflake.com/en/user-guide/python-connector-example.html#retrieving-the-snowflake-query-id
+
+
+
+print("Finished")
---
+++ sqlfile.sql
@@ -0,0 +1,4 @@
+SELECT TOP 2 * FROM mock_data;
+SELECT TOP 3 * FROM mock_data;
+SELECT TOP 5 * FROM mock_data;
+SELECT TOP 2 * FROM mock_data;
